5253313e341f5c345e4354845740f5bab1a3f4e0
jskno <lodmnos@gmail.com>
2018-11-10 13:29:16 +0100

    Starting Back End

---
+++ cryptocoins-be/src/main/java/com/jskno/cryptocoinsbe/CryptocoinsBeApplication.java
@@ -0,0 +1,12 @@
+package com.jskno.cryptocoinsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocoinsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptocoinsBeApplication.class, args);
+    }
+}
