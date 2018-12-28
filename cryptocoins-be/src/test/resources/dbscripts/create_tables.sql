cc613a7c1a311dd727fd75efc8f761cc9ac9be4f
jskno <lodmnos@gmail.com>
2018-12-29 00:45:03 +0100

    Config

--- cryptocoins-be/src/test/resources/dbscripts/create_tables.sql
+++ cryptocoins-be/src/test/resources/dbscripts/create_tables.sql
@@ -1,12 +1,3 @@
 --
--- Table structure for table `cryptocoins`
+-- Table structure for table `cryptocoins_db`
 --
-
-DROP TABLE IF EXISTS `cryptocoins`;
-
-CREATE TABLE `cryptocoins` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `ticker_symbol` varchar(15) NOT null,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
